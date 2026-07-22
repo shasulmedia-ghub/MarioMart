d3341866f67baf2c409b9ece2334640312790246
yingtong <yingtong2307@gmail.com>
2026-07-22 20:12:07 +0800

    add App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
 
     <h2>Main Body</h2>
     <p>test git push main branch</p>
+    <p>test yingtong-products branch</p>
 
     <footer className="mario-footer">
       <p className="mario-footer-credits">Copyright Capstone Project by Shahul, Johnny and YingTong</p>
