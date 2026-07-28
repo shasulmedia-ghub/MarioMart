7005348500fe49d896d3c95f910187726f30dd05
Johnny <chuaboonhock@singnet.com.sg>
2026-07-29 00:27:11 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import App from './App.jsx'
-import AdminLogin from './component/AdminLogin.jsx';
-import AdminDashboard from './component/AdminDashboard.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-      <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        
-        {/* use this to test the AdminLogin and AdminDashboard page */}
-        <Route path="/AdminLogin" element={<AdminLogin />} />
-        <Route path="/AdminDashboard" element={<AdminDashboard />} />
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>,
-)
+import App from "./App";
+
+import { AuthProvider } from "./context/AuthContext";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+
+    <AuthProvider>
+
+      <App />
+
+    </AuthProvider>
+
+  </React.StrictMode>
+);
